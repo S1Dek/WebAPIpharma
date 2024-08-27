9bf242f98261e9547a21ecc9878833843ba1b27b
marci <marci@SID-LENOVO>
2024-08-27 09:23:52 +0200

    Dodaj pliki projektów.

---
+++ Controllers/ScraperController.cs
@@ -0,0 +1,37 @@
+﻿using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using HtmlAgilityPack;
+
+namespace WebScraperAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ScraperController : ControllerBase
+    {
+        private readonly HttpClient _httpClient;
+
+        public ScraperController(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        [HttpGet("scrape")]
+        public async Task<IActionResult> Scrape([FromQuery] string url)
+        {
+            if (string.IsNullOrEmpty(url))
+                return BadRequest("URL cannot be empty");
+
+            var response = await _httpClient.GetStringAsync(url);
+
+            var htmlDocument = new HtmlDocument();
+            htmlDocument.LoadHtml(response);
+
+            // Przykład: Pobieranie elementu o ID "content" ze strony
+            var contentNode = htmlDocument.DocumentNode.SelectSingleNode("//*[@id='content']");
+            var content = contentNode != null ? contentNode.InnerHtml : "Nie znaleziono danych";
+
+            return Ok(new { Content = content });
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,31 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+// Dodanie HttpClient jako us³ugi
+builder.Services.AddHttpClient();
+
+// Dodanie us³ug MVC (obs³uga kontrolerów)
+builder.Services.AddControllers();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
